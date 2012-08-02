5c1ab3327e8a029c7526c3bfc67ee9efa50de08a
Caleb Drake <drakecb@rose-hulman.edu>
2012-08-02 12:36:39 -0400

    phone-to-phone working; attempting to implement selector for items to send

--- Contagion/src/com/contagion/ContagiousActivity.java
+++ Contagion/src/com/contagion/ContagiousActivity.java
@@ -8,98 +8,120 @@ import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class ContagiousActivity extends Activity{
-	
+
 	private NfcAdapter nfcAdapter;
 	private TextView textView;
+	private RadioGroup couponSelector;
+	private RadioButton currentCoupon;
+	private NdefRecord uriRecord; 	
 	private NdefMessage ndefMessage;
-	
-	
+	//	private String data;
+
 	@Override
-    public void onCreate(Bundle savedState) {
-        super.onCreate(savedState);
+	public void onCreate(Bundle savedState) {
+		super.onCreate(savedState);
+		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		setContentView(R.layout.contagious);
+		textView = (TextView) findViewById(R.id.ndefTextView);
+		couponSelector = (RadioGroup) findViewById(R.id.radioGroup);
+		currentCoupon = (RadioButton) findViewById(couponSelector.getCheckedRadioButtonId());
+		uriRecord = NdefRecord.createUri(currentCoupon.getText().toString());
 
-        setContentView(R.layout.contagious);
-        textView = (TextView) findViewById(R.id.ndefTextView);
-        if (nfcAdapter != null) {
-            textView.setText("Tap another Android phone with NFC to push 'NDEF Push Sample'");
-        } else {
-            textView.setText("This phone is not NFC enabled.");
-        }
-        
-//        NdefRecord uriRecord = new NdefRecord(
-//        	    NdefRecord.TNF_ABSOLUTE_URI ,
-//        	    "http://developer.android.com/index.html".getBytes(Charset.forName("US-ASCII")),
-//        	    new byte[0], new byte[0]);
-
-        NdefRecord uriRecord = NdefRecord.createUri("http://www.tetherball360.com");
-        // Create an NDEF message with some sample text
-        try {
-			ndefMessage = new NdefMessage(uriRecord.getPayload());
+		//      
+		// Create an NDEF message with some sample text
+		try {
+			ndefMessage = new NdefMessage(uriRecord.toByteArray());
 		} catch (FormatException e) {
 			e.printStackTrace();
 		}
 		
-        
-        //        new NdefRecord[] { newTextRecord("7654321", Locale.ENGLISH, true)});        
-    }
+		
+		couponSelector.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup rg, int checkedId) {
+				currentCoupon = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+				uriRecord = NdefRecord.createUri(currentCoupon.getText().toString());
+				try {
+					ndefMessage = new NdefMessage(uriRecord.toByteArray());
+				} catch (FormatException e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		if (nfcAdapter != null) {
+			textView.setText("Tap another Android phone with NFC to push 'NDEF Push Sample'");
+		} else {
+			textView.setText("This phone is not NFC enabled.");
+		}
+
+		//        NdefRecord uriRecord = new NdefRecord(
+		//        	    NdefRecord.TNF_WELL_KNOWN,
+		//        	    NdefRecord.RTD_URI, NdefRecord.getId(),"http://www.tetherball360.com"".getBytes(Charset.forName("US-ASCII));
+
+		//        NdefRecord uriRecord = NdefRecord.createUri("http://www.tetherball360.com");
+
+		//        new NdefRecord[] { newTextRecord("7654321", Locale.ENGLISH, true)});        
+	}
 
 	@Override
-    public void onResume() {
-        super.onResume();
-        if (nfcAdapter != null) nfcAdapter.setNdefPushMessage(ndefMessage, this); //(this, ndefMessage);
-    }
+	public void onResume() {
+		super.onResume();
+		if (nfcAdapter != null) nfcAdapter.setNdefPushMessage(ndefMessage, this); //(this, ndefMessage);
 
-	
-    @Override
-    public void onPause() {
-        super.onPause();
-//        if (nfcAdapter != null) nfcAdapter.disableForegroundNdefPush(this);
-    }
-	
-	
+	}
+
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		//        if (nfcAdapter != null) nfcAdapter.disableForegroundNdefPush(this);
+	}
+
+
 	public static NdefRecord newTextRecord(String text, Locale locale, boolean encodeInUtf8) {
-        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
+		byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
 
-        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
-        byte[] textBytes = text.getBytes(utfEncoding);
+		Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
+		byte[] textBytes = text.getBytes(utfEncoding);
 
-        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
-        char status = (char) (utfBit + langBytes.length);
+		int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+		char status = (char) (utfBit + langBytes.length);
 
-        byte[] data = new byte[1 + langBytes.length + textBytes.length]; 
-        data[0] = (byte) status;
-        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
-        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
-//        NdefFormatable.class
-        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
-    }
-	
-//	
-//	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.contagious);
-//		String data = "TEST NDEF!";
-//		ndefMessage = new NdefMessage(new NdefRecord[]{
-//				NdefRecord.createUri(data)
-//		});
-//		NfcAdapter.getDefaultAdapter(this).setNdefPushMessage(msg, this);
-//		
-//		// NfcAdapter adapter = manager.getDefaultAdapter();
-//		// boolean i = adapter.isEnabled();
-//		// System.out.println(i);
-//		//String id = tm.getDeviceId();
-//		//System.out.println("the tablet id is = "+id);
-//		// NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-//	  //   if (nfcAdapter == null) return;  // NFC not available on this device
-//	     
-//	  //   NdefMessage ndefMessage = nfcAdapter.setNdefPushMessage(null, null, null);
-//	     
-//	  	   	}
+		byte[] data = new byte[1 + langBytes.length + textBytes.length]; 
+		data[0] = (byte) status;
+		System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+		System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
+		//        NdefFormatable.class
+		return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
+	}
+
+	//	
+	//	public void onCreate(Bundle savedInstanceState) {
+	//		super.onCreate(savedInstanceState);
+	//		setContentView(R.layout.contagious);
+	//		String data = "TEST NDEF!";
+	//		ndefMessage = new NdefMessage(new NdefRecord[]{
+	//				NdefRecord.createUri(data)
+	//		});
+	//		NfcAdapter.getDefaultAdapter(this).setNdefPushMessage(msg, this);
+	//		
+	//		// NfcAdapter adapter = manager.getDefaultAdapter();
+	//		// boolean i = adapter.isEnabled();
+	//		// System.out.println(i);
+	//		//String id = tm.getDeviceId();
+	//		//System.out.println("the tablet id is = "+id);
+	//		// NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+	//	  //   if (nfcAdapter == null) return;  // NFC not available on this device
+	//	     
+	//	  //   NdefMessage ndefMessage = nfcAdapter.setNdefPushMessage(null, null, null);
+	//	     
+	//	  	   	}
 }
--- Contagion/src/com/contagion/MainActivity.java
+++ Contagion/src/com/contagion/MainActivity.java
@@ -1,42 +1,115 @@
 package com.contagion;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import android.annotation.SuppressLint;
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
+import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
 import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
+@SuppressLint("WorldReadableFiles")
+@SuppressWarnings("deprecation")
 public class MainActivity extends TabActivity{
+	
+	public static String TAG;
 
 		/** Called when the activity is first created. */
+
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
+			  
+			SharedPreferences opener = getPreferences(MODE_PRIVATE);
+			//opener.edit().putString("", value)
+		//	String str = opener.toString();
+		//	System.out.println(str);	
+			// String str =  myPrefs.toString();
+			//String first = "";
+	        //first = myPrefs.getString("first", null);
+	        //System.out.println(first);
+
+			
+		 //   return value;
+
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.main);			
 			TabHost tabHost = getTabHost();
-			
+						
+			 NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter();
+		     Tag tag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		     if(tag!=null) System.out.println("the tag is not null");
+		     try {
+					TAG = readTag(tag);
+		     } catch (Exception e) {
+					e.printStackTrace();
+				}
 			
 			// Tab for Home
 			TabSpec homeSpec = tabHost.newTabSpec(getString(R.string.home_title));
 			// setting Title and Icon for the Tab
 			homeSpec.setIndicator(getString(R.string.home_title),
-					getResources().getDrawable(R.drawable.ic_launcher));
+			getResources().getDrawable(R.drawable.ic_launcher));
 			Intent homeIntent = new Intent(this, HomeActivity.class);
 			homeSpec.setContent(homeIntent);
 
 			// Tab for NFC Phone-to-Phone Feature [a.k.a.	Contagious Activity]
 			TabSpec contagiousSpec = tabHost.newTabSpec(getString(R.string.contagious_title));
 			contagiousSpec.setIndicator(getString(R.string.contagious_title),
-					getResources().getDrawable(R.drawable.ic_action_search));
+			getResources().getDrawable(R.drawable.ic_action_search));
 			Intent contagiousIntent = new Intent(this, ContagiousActivity.class);
 			contagiousSpec.setContent(contagiousIntent);
-
 
 			// Adding all TabSpec to TabHost
 			tabHost.addTab(homeSpec); // Adding Home tab
 			tabHost.addTab(contagiousSpec);//Adding Find-Seat tab
-			
-
-
 		}
-	}
+		
+		@SuppressLint({ "NewApi", "NewApi", "NewApi", "NewApi" })
+		public String readTag(Tag tag) throws IOException {
+	        MifareUltralight mifare = MifareUltralight.get(tag);
+	        	try {
+					mifare.connect();
+					byte[] payload = mifare.readPages(4);
+					String s =  new String(payload, Charset.forName("ASCII"));
+					s = s.substring(9,16);
+					//System.out.println(s);
+					return s;
+	        	} catch (IOException e) {
+	        }
+	        finally {
+	            if (mifare != null) {
+	            	try {
+	                   mifare.close();
+	            	}
+	            	catch (IOException e) {}
+	            }
+	        }
+	        return null;
+	    }	
+
+
+	//	private boolean first_time_check() {
+	        /* 
+	         * Checking Shared Preferences if the user had pressed 
+	         * the remember me button last time he logged in
+	         * */
+	     /*   String first = uPreferences.getString("first", null);
+	        if((first == null)){
+	            return false;
+	        }
+	        else 
+	            return true;
+	    }
+*/
+
+
+
+}
